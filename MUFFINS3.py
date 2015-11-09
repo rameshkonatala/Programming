1de47c4f6724d051ddf6b9d81b5e3e96bd52352a
ramesh konatala <konatalaramesh@gmail.com>
2015-11-09 17:07:15 +0530

    modified muffins3 and optimised it

--- MUFFINS3.py
+++ MUFFINS3.py
@@ -1,25 +1,12 @@
-def count(x):
-	a=[]
-	for i in range(1,x+1):
-		a.append(x%i)
-	p=max(a)
-	t=[]
-	for i in range(len(a)):
-		if a[i]==p:
-			t.append(i+1)
-	return max(t)	
-			
-			
 t=int(raw_input())
 b=[]
 for i in range(t):
 	n=int(raw_input())
-	b.append(count(n))
+	if n%2==0:
+		b.append(n-(n/2)+1)
+	else:
+		b.append(n-(n/2))
 
-	
 for i in range(t):
 	print b[i]
 	
-	
-
-		
