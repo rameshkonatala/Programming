cbbd78b889bffe7d7f3aafc79453bf80601ae511
ramesh konatala <konatalaramesh@gmail.com>
2015-11-09 21:15:29 +0530

    added permut2

--- PERMUT2.py
+++ PERMUT2.py
@@ -1,31 +1,20 @@
-def ambiguous(x):
-	p=[]
-	t=0
-	for i in range(len(x)):
-		p.append(str(x.index(str(i+1))+1))
+while(1):
+	n = int(input())
+	if n==0:
+		break
+	
 		
-		if p[i]!=x[i]:
-			t=1
+	a=[int(x) for x in raw_input().split()]
+	t=1
+
+	for i in range(len(a)):
+		if a[a[i]-1]!=i+1:
+			t=2
 			break
-	
-	if t==0:
-		return 1	
-	else:
-		return 2
-
-		
-p=int(raw_input())
-b=[]
-while p!=0:
-	a=raw_input().split()
-	
-	q= ambiguous(a)
-	b.append(q)
-	p=int(raw_input())
-
-	
-for i in range(len(b)):
-	if b[i]==1:
-		print "ambiguous"	
-	else:
+			
+	if t==2:
 		print "not ambiguous"
+	else:
+		print "ambiguous"
+
+	
