3c89754b5f6510e56288c1151652de8c8690d66c
ramesh konatala <konatalaramesh@gmail.com>
2015-11-17 13:20:14 +0530

    modified file

--- TOANDFRO.py
+++ TOANDFRO.py
@@ -1,8 +1,20 @@
-t=int(raw_input())
-a=raw_input().split()
-b=[]
-j=0
-while j<
-while j<len(a):
-	b.append(a[j])
-	j+=t
+r=int(raw_input())
+while r!=0:
+	a=[h for h in raw_input()]
+	k=1
+	x=[]
+	while k<=r:
+		i=k
+		j=(2*r)-k
+		while i<=len(a):
+			x.append(a[i-1])
+			if j<len(a):
+				x.append(a[j])
+			i=i+(2*r)
+			j+=(2*r)
+			
+		k=k+1
+	print "".join(x)
+	r=int(raw_input())
+		
+		
