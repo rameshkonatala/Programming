265a80631997f48cfbc9c73aa12fe033fe58b917
ramesh konatala <konatalaramesh@gmail.com>
2015-11-18 12:16:05 +0530

    adding new files

--- AE00.py
+++ AE00.py
@@ -1,16 +1,21 @@
 t=int(raw_input())
 l=1
-for i in range(t):
-	if t<i*i:
-		l=i
-		break
-
-c=0		
-for j in range(1,l):
-	for k in range(j,t+1):
-		if j*k>t:
+if t==1:
+	print 1
+elif t==2:
+	print 2
+else:
+	for i in range(t):
+		if t<i*i:
+			l=i
 			break
-		else:
-			c=c+1
 
-print c
+	c=0		
+	for j in range(1,l):
+		for k in range(j,t+1):
+			if j*k>t:
+				break
+			else:
+				c=c+1
+
+	print c
