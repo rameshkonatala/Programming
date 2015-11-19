a5fbf69e463d23db7d3e89fdfb8d56b3a4ae3cd3
ramesh konatala <konatalaramesh@gmail.com>
2015-11-19 18:40:20 +0530

    modified file

--- CANTON.py
+++ CANTON.py
@@ -2,14 +2,15 @@ t=int(raw_input())
 for i in range(t):
 	k=int(raw_input())
 	if k==1:
-		print "TERM",k,"IS",str(1)+str("/")+str(1)
+		
+		print "TERM %d IS 1/1" %(k)
 	else:
 		n=1
 		while (n*(n+1))/2 < k:
 			s=(n*(n+1))/2
 			n=n+1
 		a=k-s
-		if n%2==0:
-			print "TERM",k,"IS",str(n+1-a)+str("/")+str(a)
+		if n%2!=0:
+			print "TERM %d IS %d/%d" %(k,n+1-a,a)
 		else:
-			print "TERM",k,"IS",str(a)+str("/")+str(n+1-a)
+			print "TERM %d IS %d/%d" %(k,a,n+1-a)
