5e08aed6134c5fa854527a2fbd7a847bb6eeff04
ramesh konatala <konatalaramesh@gmail.com>
2015-11-19 23:29:35 +0530

    modified file

--- WILLITST.py
+++ WILLITST.py
@@ -1,15 +1,13 @@
-#t=int(raw_input())
-for i in range(100):
-	t=i+1
-	s=1
-	a=0
-	while 2**s<=t:
-		if 2**s==t:
-			a=1
-			break
-		else:
-			s=s+1
-	if a==0:
-		print t,"NIE"
+t=int(raw_input())
+s=1
+a=0
+while 2**s<=t:
+	if 2**s==t:
+		a=1
+		break
 	else:
-		print t,"TAK"
+		s=s+1
+if a==0:
+	print "NIE"
+else:
+	print "TAK"
