a1255d534b7fd038fa8091f4266005b761499337
ramesh konatala <konatalaramesh@gmail.com>
2015-11-30 14:57:01 +0530

    modified file

--- PALIN.py
+++ PALIN.py
@@ -1,20 +1,24 @@
-def palin(x):
-	a=[int(i) for i in str(x)]
-	c=0
-	for i in range(len(a)/2):
-		if a[i]!=a[len(a)-i-1]:
-			c=2
-			break
-	return c	
-
+def palin(a):
+	
+	if len(a)<=1:
+		return True
+		
+	elif a[0]!=a[len(a)-1]:
+		return False
+		
+	else:
+		a.pop()
+		a.pop(0)
+		return palin(a) 
+		
 t=int(raw_input())
 for i in range(t):
 	p=int(raw_input())+1
 	t=1
 	while t!=0:
-		if palin(p)==0:
+		if palin([j for j in str(p)]):
 			print p
 			t=0
 		else:
+			
 			p=p+1
-	
