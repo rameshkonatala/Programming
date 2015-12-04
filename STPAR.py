af2a646732b371dde537afb8d426353885581fe4
ramesh konatala <konatalaramesh@gmail.com>
2015-12-04 18:12:22 +0530

    modified file

--- STPAR.py
+++ STPAR.py
@@ -1,25 +1,32 @@
 p=int(raw_input())
-while p!=0: 
-	a=map(int,raw_input().split())
-	t=0
+while p!=0:
+	to=[]
+	bet=[]
+	fro=map(int,raw_input().split())
 	j=0
-	k=1
-	top=p+1
-	while j<len(a) and t==0:
-		if k!=a[j] and top>a[j]:
-			top=a[j]
-			
-		elif len(c)>0 and k==c(len(c)-1):
-			c.append(a[j])
-			if len(c)>1 and c[len(c)-2]<c[len(c)-1]:
-				t=1
+	fro.reverse()
+	x=len(fro)
+	t=1
+	while t!=0 and len(to)!=x:
+		if len(fro)!=0 and fro[len(fro)-1]==(j+1):
+			to.append(fro.pop())
+			j=j+1
+			#print "from fro to to",to
+		elif len(bet)!=0 and bet[len(bet)-1]==(j+1):
+			to.append(bet.pop())
+			j=j+1
+			#print "from bet to to",to
 		else:
-			k=k+1
+			if len(fro)!=0:
+				bet.append(fro.pop())
+				#print "from fro to bet",bet
+			if len(bet)>1 and bet[len(bet)-1]>bet[len(bet)-2]:
+				t=0
 		
-		j=j+1
 
 	if t==0:
-		print "Yes"
-	else:
 		print "No"
-	p=int(raw_input())
+	else:
+		print "Yes"
+
+	p=int(raw_input())	
